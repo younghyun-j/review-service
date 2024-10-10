c064d94f7eb0db4f563f33ef782f745e2492f5b5
younghyun.j <zerohyun.j@gmail.com>
2024-10-10 15:16:10 +0900

    [feat] ProductReviewController 구현 #5
    * local 환경에서 사용할 더미 sql 추가 및 gitignore 설정

---
+++ src/main/java/com/eccomerce/api/product/controller/ProductReviewController.java
@@ -0,0 +1,31 @@
+package com.eccomerce.api.product.controller;
+
+import com.eccomerce.api.image.service.ImageService;
+import com.eccomerce.api.product.dto.request.CreateProductReviewRequest;
+import com.eccomerce.api.product.dto.response.ProductReviewListResponse;
+import com.eccomerce.api.product.service.ProductReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductReviewController {
+
+    private final ProductReviewService productReviewService;
+    private final ImageService imageService;
+
+    @GetMapping("/products/{productId}/reviews")
+    public ResponseEntity<Void> findReviews(@PathVariable Long productId,
+                                            @ModelAttribute @Valid CreateProductReviewRequest request) {
+        // 이미지 업로드
+        String uploadImageUrl = "";
+        if (request.image() != null) {
+            uploadImageUrl = imageService.imageUpload(request.image());
+        }
+        productReviewService.saveProductReview(productId, request, uploadImageUrl);
+        return ResponseEntity.ok().build();
+    }
+
+}
